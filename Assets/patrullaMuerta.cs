8dd677b99fc4e51b681c4618f95d679f95ddac2f
Lucyan <Levyathan666@gmail.com>
2023-09-12 23:34:34 -0500

    NavMesh Muertita

---
+++ Assets/patrullaMuerta.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class patrullaMuerta : MonoBehaviour
+{
+    GameObject player;
+    private NavMeshAgent nav;
+    [SerializeField] bool playerNear;
+    [SerializeField] float speedChase;
+    Animator anim;
+
+    [SerializeField] float patrolSpeed;
+    [SerializeField] float patrolTimer;
+    [SerializeField] float patrolWaitingTime;
+    public Transform[] wayPoints;
+    int wayPointsIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();    
+        anim = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerNear)
+        {
+            nav.destination = player.transform.position;
+            nav.speed = speedChase;
+            
+            if(nav.velocity == Vector3.zero)
+            {
+                anim.SetBool("walk", false);
+            }
+            else
+            {
+                anim.SetBool("walk", true);
+            }
+        }
+
+        Patrolling();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            playerNear = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerNear = false;
+            anim.SetBool("walk", false);
+        }
+    }
+
+    public void Patrolling()
+    {
+        nav.speed = patrolSpeed;
+        nav.stoppingDistance = 1;
+
+        if (nav.remainingDistance < nav.stoppingDistance)
+        {
+            patrolTimer += Time.deltaTime;
+
+            if (patrolTimer > patrolWaitingTime)
+            {
+                if (wayPointsIndex == wayPoints.Length - 1)
+                {
+                    wayPointsIndex = 0;
+                }
+                else
+                {
+                    patrolTimer = 0;
+                }
+            }
+
+            else
+            {
+                patrolTimer = 0;
+            }
+            nav.destination = wayPoints[wayPointsIndex].position;
+        }
+    }    
+}
