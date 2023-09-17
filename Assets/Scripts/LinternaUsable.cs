8a01510910f0b568bc45fc84d4d83595f7b65b58
Luz Fernández Acuña <luz.fernandezac@gmail.com>
2023-09-17 10:27:42 -0500

    adaptation batteries in multiples habitations

--- Assets/Scripts/LinternaUsable.cs
+++ Assets/Scripts/LinternaUsable.cs
@@ -190,7 +190,7 @@ public class LinternaUsable : MonoBehaviour
             // Puedes destruir el objeto de la batería si lo deseas
             //Destroy(other.gameObject);
             Debug.Log($"aca vamos pal respaun {other.gameObject.name}");
-            other.gameObject.GetComponentInParent<SpawnBaterias>().RespawnBateria(transform.position);
+            GameObject.Find("ControladorBatería").GetComponentInParent<SpawnBaterias>().RespawnBateria(transform.position);
         }
     }
 
